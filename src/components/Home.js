6e145820c6cd07c24060b0481bb60191a583e7be
Liu Ziyi <liuziyi@Lius-MacBook-Pro.local>
2018-07-22 01:12:01 +0800

    edit Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,6 @@ class Home extends Component {
   render(){
     return(
       <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-          <h1 className="display-4" style={{ marginBottom: '40px' }}>Statistics</h1>
           <p className="lead">MAU: 1 Billion</p>
           <p className="lead">REV: 10 Billion</p>
 
